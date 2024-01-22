19bf2b8c98554f1aaa5e715c06db6a03ded592fd
mrkroms <spklgd@gmail.com>
2024-01-22 10:19:29 +0200

    add charList component

--- marvel_starter/src/components/randomChar/RandomChar.js
+++ marvel_starter/src/components/randomChar/RandomChar.js
@@ -22,12 +22,17 @@ class RandomChar extends Component {
     this.setState({ char, loading: false });
   };
 
+  onCharLoading = () => {
+    this.setState({ loading: true });
+  };
+
   onError = () => {
     this.setState({ loading: false, error: true });
   };
 
   updateChar = () => {
     let id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+    this.onCharLoading();
     this.marvelService
       .getCharacter(id)
       .then(this.onCharLoaded)
@@ -52,7 +57,7 @@ class RandomChar extends Component {
             Do you want to get to know him better?
           </p>
           <p className="randomchar__title">Or choose another one</p>
-          <button className="button button__main">
+          <button onClick={this.updateChar} className="button button__main">
             <div className="inner">try it</div>
           </button>
           <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
@@ -64,9 +69,22 @@ class RandomChar extends Component {
 
 const View = ({ char }) => {
   const { name, description, thumbnail, homepage, wiki } = char;
+  let imgStyle = { objectFit: "cover" };
+  if (
+    thumbnail ===
+    "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+  ) {
+    imgStyle = { objectFit: "contain" };
+  }
+
   return (
     <div className="randomchar__block">
-      <img src={thumbnail} alt="Random character" className="randomchar__img" />
+      <img
+        src={thumbnail}
+        alt="Random character"
+        className="randomchar__img"
+        style={imgStyle}
+      />
       <div className="randomchar__info">
         <p className="randomchar__name">{name}</p>
         <p className="randomchar__descr">{description}</p>
@@ -75,7 +93,7 @@ const View = ({ char }) => {
             <div className="inner">homepage</div>
           </a>
           <a href={wiki} className="button button__secondary">
-            <div className="inner">wiki</div>
+            <div className="inner">Wiki</div>
           </a>
         </div>
       </div>
