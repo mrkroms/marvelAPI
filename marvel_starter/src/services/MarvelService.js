6f199d49a37cdd0b4beafe71eeb75c36819deb52
mrkroms <spklgd@gmail.com>
2024-01-22 07:26:57 +0200

    add randomChar component

--- marvel_starter/src/services/MarvelService.js
+++ marvel_starter/src/services/MarvelService.js
@@ -11,16 +11,30 @@ class MarvelService {
     return await res.json();
   };
 
-  getAllCharacters = () => {
-    return this.getRecource(
+  getAllCharacters = async () => {
+    const res = await this.getRecource(
       `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
     );
+    return res.data.results.map(this._transformCharacter);
   };
 
-  getCharacter = (id) => {
-    return this.getRecource(
+  getCharacter = async (id) => {
+    const res = await this.getRecource(
       `${this._apiBase}characters/${id}?&${this._apiKey}`
     );
+    return this._transformCharacter(res.data.results[0]);
+  };
+  _transformCharacter = (char) => {
+    return {
+      name: char.name,
+      description:
+        char.description === ""
+          ? `'There is no description for this character'`
+          : `${char.description.slice(0, 210)}...`,
+      thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+    };
   };
 }
 
